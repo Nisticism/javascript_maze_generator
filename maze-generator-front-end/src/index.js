5f8ec9a8043d52654e3bef80fe3ca40aa3ce57b1
nisticism <nistacular@gmail.com>
2020-08-15 14:17:21 -0700

    UI upgrades

--- maze-generator-front-end/src/index.js
+++ maze-generator-front-end/src/index.js
@@ -1,5 +1,6 @@
 const BASE_URL = "http://localhost:3000";
 const MAZES = `${BASE_URL}/mazes`;
+const PROFILE = `${BASE_URL}/user`;
 
 const main = document.querySelector("main");
 document.addEventListener("DOMContentLoaded", () => loadMaze(4));
@@ -21,15 +22,93 @@ const loadMaze = (id) => {
     });
 };
 
+const loadUser = (id) => {
+  fetch(PROFILE)
+    .then((res) => res.json())
+    .then((json) => {
+      renderUser(json[id]);
+    });
+};
+
+const renderProfile = (userHash) => {
+  const div = document.createElement("div");
+  const p = document.createElement("p");
+  const button = document.createElement("button");
+  const ul = document.createElement("ul");
+
+  div.setAttribute("class", "card");
+  div.setAttribute("data-id", userHash.id);
+  p.innerHTML = userHash.name;
+
+  button.setAttribute("data-trainer-id", trainerHash.id);
+  button.innerHTML = "Play Game";
+  button.addEventListener("click", createPokemon);
+
+  div.appendChild(p);
+  div.appendChild(button);
+  div.appendChild(ul);
+
+  main.appendChild(div);
+  trainerHash.pokemons.forEach((pokemon) => renderPokemon(pokemon));
+};
+
+const renderMazes = (mazesHash) => {};
+
 const renderMaze = (mazeHash) => {
+
+  const canvas_div = document.createElement("div");
+  const generate_div = document.createElement("div");
+  const log_in_div = document.createElement("div");
+
   const canvas = document.createElement("canvas");
+  const ruler = document.createElement("img");
+
+  const generate_maze_button = document.createElement("button");
+  const generate_maze_width_field = document.createElement("input");
+  const generate_maze_height_field = document.createElement("input");
+  const play_button = document.createElement("button");
+
+  const log_in_button = document.createElement("button");
+  const username_field = document.createElement("input");
+
+  canvas_div.setAttribute("id", "canvas_div");
+  generate_div.setAttribute("id", "generate_div");
+  log_in_div.setAttribute("id", "log_in_div");
 
   canvas.setAttribute("id", "canvas");
   canvas.setAttribute("class", "canvas");
   canvas.setAttribute("width", mazeHash.width * 10);
   canvas.setAttribute("height", mazeHash.height * 10);
+  ruler.setAttribute("id", "ruler");
+  ruler.setAttribute("src", "src/400px_ruler.png");
+  ruler.setAttribute("alt", "ruler");
+  ruler.setAttribute("width", "411");
+  ruler.setAttribute("height", "30");
 
-  main.appendChild(canvas);
+  generate_maze_button.setAttribute("id", "generate");
+  generate_maze_button.innerHTML = "Generate maze";
+  generate_maze_width_field.setAttribute("id", "maze_width_field");
+  generate_maze_height_field.setAttribute("id", "maze_height_field");
+  play_button.setAttribute("id", "play_button");
+
+  log_in_button.setAttribute("id", "log_in");
+  log_in_button.innerHTML = "Log In";
+  username_field.setAttribute("id", "log_in_field");
+
+  canvas_div.appendChild(canvas);
+  canvas_div.appendChild(ruler);
+
+  generate_div.appendChild(generate_maze_button);
+  generate_div.appendChild(generate_maze_width_field);
+  generate_div.appendChild(generate_maze_height_field);
+  generate_div.appendChild(play_button);
+
+  log_in_div.appendChild(log_in_button);
+  log_in_div.appendChild(username_field);
+
+  main.appendChild(canvas_div);
+  main.appendChild(generate_div);
+  main.appendChild(log_in_div);
 };
 
 function draw() {
@@ -43,3 +122,5 @@ function draw() {
     ctx.fill(circle);
   }
 }
+
+function check_if_logged_in() {}
